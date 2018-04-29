2f3a76bb6097c932906f747a8f6e0310bb390037
cindych419@gmail.com <cindych419@gmail.com>
2018-04-29 21:52:09 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -7,11 +7,11 @@ end
   "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep(number)
-  while number > 0
-    puts "#{number} SECOND(S)!"
-    number -= 1
-  sleep(1.minute)
+def countdown_with_sleep(num_secs)
+  while num_secs > 0
+    puts "#{num_secs} SECOND(S)!"
+    num_secs -= 1
+  sleep(1)
 end
   "HAPPY NEW YEAR!"
 end
