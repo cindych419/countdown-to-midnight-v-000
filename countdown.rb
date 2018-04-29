03beee7a74df00ea86af5be96277c7db5fa34875
cindych419@gmail.com <cindych419@gmail.com>
2018-04-29 21:12:04 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -2,9 +2,10 @@
 
 def countdown(number)
   number = 12
+  puts "#{number} SECOND(S)!"
   while number = 0
-    puts "#{number} SECOND(S)!""
-    "HAPPY NEW YEAR!"
+    puts "#{number} SECOND(S)!"
     x -= 1
+    "HAPPY NEW YEAR!"
 end
 end
